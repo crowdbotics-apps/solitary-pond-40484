db80ebf2c42f62f54715f0858953a20e9ae36044
Crowdbotics <team@crowdbotics.com>
2023-05-05 18:45:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-pond-40484",
+  url: "https://solitary-pond-40484.botics.co",
+  api: "https://solitary-pond-40484.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
